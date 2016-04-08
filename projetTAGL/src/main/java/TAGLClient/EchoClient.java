f7d4de7a87139f78dc4c34a38d5d3391063951b4
lakse <lakse@127.0.0.1>
2016-04-08 12:09:31 +0200

    modif client & serveur

--- projetTAGL/src/main/java/TAGLClient/EchoClient.java
+++ projetTAGL/src/main/java/TAGLClient/EchoClient.java
@@ -1,29 +1,73 @@
 package TAGLClient;
+import java.io.BufferedReader;
 
 import java.io.IOException;
+
+import java.io.InputStreamReader;
+
+import java.io.PrintWriter;
+
 import java.net.InetAddress;
+
 import java.net.Socket;
+
 import java.net.UnknownHostException;
 
 
-public class EchoClient{
-	
-	public static void main(String[] zero) {
-		
-		Socket socket;
 
-		try {
-		
-		     socket = new Socket(InetAddress.getLocalHost(),2009);	
-	             socket.close();
+public class EchoClient {
 
-		}catch (UnknownHostException e) {
-			
-			e.printStackTrace();
-		}catch (IOException e) {
-			
-			e.printStackTrace();
-		}
-	}
+    
+
+    public static void main(String[] zero) {
+
+        
+
+        
+
+        Socket socket;
+
+        BufferedReader in;
+
+        PrintWriter out;
+
+
+        try {
+
+        
+
+            socket = new Socket(InetAddress.getLocalHost(),2009);   
+
+                System.out.println("Demande de connexion");
+
+
+                in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+
+                String message_distant = in.readLine();
+
+                System.out.println(message_distant);
+
+                
+
+                socket.close();
+
+               
+
+        }catch (UnknownHostException e) {
+
+            
+
+            e.printStackTrace();
+
+        }catch (IOException e) {
+
+            
+
+            e.printStackTrace();
+
+        }
+
+    }
+
 
 }
