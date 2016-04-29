6fd7da17bcdcab8c95ef922a91ddc67c98315c7c
lakse <lakse@127.0.0.1>
2016-04-29 11:28:34 +0200

    Test multi thread

--- projetTAGL/src/main/java/TAGLClient/EchoClient.java
+++ projetTAGL/src/main/java/TAGLClient/EchoClient.java
@@ -16,79 +16,120 @@ import java.net.UnknownHostException;
 
 
 
+//public class EchoClient {
+//
+//    
+//
+//    public static void main(String[] zero) throws IOException {
+//
+//        
+//
+//        Socket socket;
+//
+//        BufferedReader in;
+//
+//        PrintWriter out;
+//        /*
+//        BufferedReader br;
+//		try {
+//			br = new BufferedReader(new FileReader("properties/projetTAGL/src/main/java/TAGLClient/input.txt"));
+//		} catch (FileNotFoundException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//        try {
+//            StringBuilder sb = new StringBuilder();
+//            String line = br.readLine();
+//
+//            while (line != null) {
+//                sb.append(line);
+//                sb.append(System.lineSeparator());
+//                line = br.readLine();
+//            }
+//            String everything = sb.toString();
+//        } finally {
+//            br.close();
+//        }
+//        */
+//        String commande = "lPush lecture 4  \n incr 4 \n ei \n";
+//
+//        try {
+//        		socket = new Socket(InetAddress.getLocalHost(),2009);
+//        		out = new PrintWriter(socket.getOutputStream(), true);
+//
+//        		//socket = new Socket(InetAddress.getLocalHost(),2009);
+//                System.out.println("Demande de connexion");
+//
+//
+//                in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+//        		String commande= in.readLine();
+//                String message_distant = in.readLine();
+//
+//                System.out.println(message_distant);
+//                out.println(commande);
+//                
+//
+//                socket.close();
+//
+//               
+//
+//        }catch (UnknownHostException e) {
+//
+//            
+//
+//            e.printStackTrace();
+//
+//        }catch (IOException e) {
+//
+//            
+//
+//            e.printStackTrace();
+//
+//        }
+//
+//    }
+//
+//
+//}
+
 public class EchoClient {
 
+    BufferedReader in;
+    PrintWriter out;
     
-
-    public static void main(String[] zero) throws IOException {
-
-        
-
-        Socket socket;
-
-        BufferedReader in;
-
-        PrintWriter out;
-        /*
-        BufferedReader br;
-		try {
-			br = new BufferedReader(new FileReader("properties/projetTAGL/src/main/java/TAGLClient/input.txt"));
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-        try {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
-
-            while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-                line = br.readLine();
-            }
-            String everything = sb.toString();
-        } finally {
-            br.close();
-        }
-        */
-        String commande = "lPush lecture 4  \n incr 4 \n ei \n";
-
-        try {
-        		socket = new Socket(InetAddress.getLocalHost(),2009);
-        		out = new PrintWriter(socket.getOutputStream(), true);
-
-        		//socket = new Socket(InetAddress.getLocalHost(),2009);   
-
-                System.out.println("Demande de connexion");
-
-
-                in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
-
-                String message_distant = in.readLine();
-
-                System.out.println(message_distant);
-                out.println(commande);
-                
-
-                socket.close();
-
-               
-
-        }catch (UnknownHostException e) {
-
-            
-
-            e.printStackTrace();
-
-        }catch (IOException e) {
-
-            
-
-            e.printStackTrace();
-
-        }
+    public EchoClient() {
+        in= null;
+        out=null;
 
     }
+   
+    private void run() throws IOException {
+    	InetAddress serverAddress=InetAddress.getLocalHost();
+        try(
+        Socket socket = new Socket(serverAddress, 2009);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader stdIn =
+                new BufferedReader(
+                    new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                System.out.println(in.readLine());
+                out.println(userInput);
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + serverAddress);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                serverAddress);
+            System.exit(1);
+        }
+    }
 
-
+    public static void main(String[] args) throws Exception {
+        EchoClient client = new EchoClient();
+        client.run();
+    }
 }
--- projetTAGL/src/main/java/TAGLServer/EchoServer.java
+++ projetTAGL/src/main/java/TAGLServer/EchoServer.java
@@ -1,67 +1,176 @@
 package TAGLServer;
 import java.io.BufferedReader;
-
 import java.io.IOException;
-
 import java.io.InputStreamReader;
-
 import java.io.PrintWriter;
-
-import java.net.InetAddress;
-
 import java.net.ServerSocket;
-
 import java.net.Socket;
-
-import java.net.UnknownHostException;
-
-import java.io.PrintWriter;
+import java.util.HashSet;
 
 import TAGLClient.TableHachage;
+
+//public class EchoServer {
+//
+//    
+//
+//    public static void main(String[] zero) {
+//
+//        
+//
+//        ServerSocket socketserver  ;
+//
+//        Socket socketduserveur ;
+//
+//        BufferedReader in;
+//
+//        PrintWriter out;
+//
+//        try {
+//
+//            socketserver = new ServerSocket(2009);
+//
+//            System.out.println("Le serveur est à l'écoute du port "+socketserver.getLocalPort());
+//
+//            socketduserveur = socketserver.accept(); 
+//
+//                System.out.println("Un utilisateur s'est connecté");
+//
+//            out = new PrintWriter(socketduserveur.getOutputStream());
+//
+//                out.println("Vous êtes connecté");
+//
+//                out.flush();
+//                /***/
+//                // creation table hachage
+//                TableHachage tableHachage = new TableHachage();
+//                //buffer permettant de lire les messages du/des client(s)
+//                BufferedReader inCl = new BufferedReader( 
+//                        new InputStreamReader( socketduserveur.getInputStream())); 
+//
+//                String inputLine; 
+//                while ((inputLine = inCl.readLine()) != null) 
+//                    { 
+//                	if(inputLine.length() > 0){
+//                		// enleve un eventuel espace au debut de la commande
+//	                	if(inputLine.charAt(0)==' '){
+//	                		StringBuilder strB = new StringBuilder(inputLine);
+//	                	    strB.deleteCharAt(0);
+//	                	    inputLine = strB.toString();
+//	                	}
+//                	}
+//                     System.out.println ("Server: " + inputLine);                      
+//                     out.println(inputLine);
+//                     
+//                     if( inputLine.contains("lPush" ) ) {
+//                    	 // split afin de recuperer les arguments de la fonction
+//                    	 String[] parts = inputLine.split(" ");
+//                    	 tableHachage.lPush(parts[1],parts[2]);
+//                     }
+//                     else if (inputLine.contains("rPush" )){
+//                    	 String[] parts = inputLine.split(" ");
+//                    	 tableHachage.rPush(parts[1],parts[2]);
+//                     }
+//                     else if (inputLine.contains("lRange" )){
+//                    	 String[] parts = inputLine.split(" ");
+//                    	 tableHachage.lRange(parts[1],Integer.parseInt(parts[2]),Integer.parseInt(parts[3]));
+//                     }
+//                     else if (inputLine.contains("lLen" )){
+//                    	 String[] parts = inputLine.split(" ");
+//                    	 System.out.println("La taille de la table est de : " +tableHachage.lLen(parts[1]));
+//                     }
+//                     
+//                     else if (inputLine.contains("set" )){
+//                    	 String[] parts = inputLine.split(" ");
+//                    	 tableHachage.set(parts[1],parts[2]);
+//                     }
+//                     else if (inputLine.contains("get" )){
+//                    	 String[] parts = inputLine.split(" ");
+//                    	 tableHachage.get(parts[1]);
+//                     }
+//                     else if (inputLine.contains("del" )){
+//                    	 String[] parts = inputLine.split(" ");
+//                    	 tableHachage.del(parts[1]);
+//                     }
+//                     else if (inputLine.contains("incr" )){
+//                    	 String[] parts = inputLine.split(" ");
+//                    	 tableHachage.incr(parts[1]);
+//                     }
+//                     else{
+//                    	 System.out.println("Mauvaise commande");         
+//                     }
+//
+//                     if (inputLine.equals("Bye.")) 
+//                         break; 
+//                    } 
+//                /****/
+//                socketduserveur.close();
+//
+//                socketserver.close();
+//
+//                
+//
+//        }catch (IOException e) {
+//
+//            
+//
+//            e.printStackTrace();
+//
+//        }
+//
+//    }
+//
+//
+//}
 
 public class EchoServer {
 
     
-
-    public static void main(String[] zero) {
-
-        
-
-        ServerSocket socketserver  ;
-
-        Socket socketduserveur ;
-
-        BufferedReader in;
-
-        PrintWriter out;
-
+    private static final int PORT = 2009;
+    private static HashSet<String> names = new HashSet<String>();
+    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("The server is running.");
+        ServerSocket listener = new ServerSocket(PORT);
         try {
+            while (true) {
+                new Handler(listener.accept()).start();
+            }
+        } finally {
+            listener.close();
+        }
+    }
 
-            socketserver = new ServerSocket(2009);
+        private static class Handler extends Thread {
+        private String name;
+        private Socket socket;
+        private BufferedReader in;
+        private PrintWriter out;
+        private TableHachage tableHachage;
 
-            System.out.println("Le serveur est à l'écoute du port "+socketserver.getLocalPort());
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
 
-            socketduserveur = socketserver.accept(); 
+        public void run() {
+            try {
 
-                System.out.println("Un utilisateur s'est connecté");
+                in = new BufferedReader(new InputStreamReader(
+                socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+                tableHachage = new TableHachage();
 
-            out = new PrintWriter(socketduserveur.getOutputStream());
+                synchronized (names) {
+                    name = "client " + names.size()+"";
+                    names.add(name);
+                }
+                
+                writers.add(out);
 
-                out.println("Vous êtes connecté");
-
-                out.flush();
-                /***/
-                // creation table hachage
-                TableHachage tableHachage = new TableHachage();
-                //buffer permettant de lire les messages du/des client(s)
-                BufferedReader inCl = new BufferedReader( 
-                        new InputStreamReader( socketduserveur.getInputStream())); 
-
-                String inputLine; 
-                while ((inputLine = inCl.readLine()) != null) 
-                    { 
+                while (true && !writers.isEmpty()) {
+                	String inputLine=in.readLine();
                 	if(inputLine.length() > 0){
-                		// enleve un eventuel espace au debut de la commande
+               		// enleve un eventuel espace au debut de la commande
 	                	if(inputLine.charAt(0)==' '){
 	                		StringBuilder strB = new StringBuilder(inputLine);
 	                	    strB.deleteCharAt(0);
@@ -111,23 +220,26 @@ public class EchoServer {
 
                      if (inputLine.equals("Bye.")) 
                          break; 
-                    } 
-                /****/
-                socketduserveur.close();
-
-                socketserver.close();
-
-                
-
-        }catch (IOException e) {
-
-            
-
-            e.printStackTrace();
-
+                    }
+//                    String input = in.readLine();
+//                    if(input==null){
+//                        return;
+//                    }
+//                    System.out.println(name + ": " + input);
+            } catch (IOException e) {
+                System.out.println(e);
+            } finally {
+                if (name != null) {
+                    names.remove(name);
+                }
+                if (out != null) {
+                    writers.remove(out);
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
         }
-
     }
-
-
 }
