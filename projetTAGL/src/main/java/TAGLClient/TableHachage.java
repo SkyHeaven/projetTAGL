306ee966c859c5dcb7bb71497688b5b1ef3e959b
Jypay <grosje@127.0.0.1>
2016-04-29 11:15:35 +0200

    Modif tablehachage et realisation des junit test pour tablehachage

--- projetTAGL/src/main/java/TAGLClient/TableHachage.java
+++ projetTAGL/src/main/java/TAGLClient/TableHachage.java
@@ -11,22 +11,67 @@ public class TableHachage {
 		h = new HashMap<Object, Object>();
 	}
 	
-	public void set(Object k, Object v){
-		h.put(k, v);
+	public int valueOfString(Object v){
+		int res = -1;
+		if (v instanceof String){
+			String st = v.toString(); 
+			int i =0;
+			/*
+			 * res=1 si float
+			 * res=0 si int
+			 * res=-1 si non reel
+			 */
+			if (st.charAt(i)=='+' || st.charAt(i)=='-'){
+				i++;
+			}
+			System.out.println(i);
+			res=0;
+			for (i=i;i<st.length();i++){
+				if(st.charAt(i)=='.' && res!=1){
+					res =1;
+				}
+				else if(st.charAt(i)=='.' && res==1){
+					res=-1;
+					break;
+				}
+				else if (!(st.charAt(i)>='0' && st.charAt(i)<='9')){
+					res = -1;
+					break;
+				}
+			}
+		}
+		return res;
 	}
 	
-	public void get(Object k){
+	public void set(Object k, Object v){
+		if (valueOfString(v) == 0){
+			int res = Integer.parseInt((String)v);
+			h.put(k, res);
+		}
+		else if (valueOfString(v) == 1){
+			Double res = Double.parseDouble((String)v);
+			System.out.println(res);
+			h.put(k, res);
+		}
+		else {
+			h.put(k, v);
+		}
+	}
+	
+	public Object get(Object k){
 		if (h.containsKey(k)){
-			h.get(k);
+			return h.get(k);
 		}
 		else {
 			System.out.println("La clé n'existe pas");
+			return 0;
 		}
 	}
 	
 	public void del(Object k){
 		if (h.containsKey(k)){
 			h.remove(k);
+			System.out.println("La clé a été supprimé");
 		}
 		else {
 			System.out.println("La clé n'existe pas");
@@ -113,9 +158,12 @@ public class TableHachage {
 					}
 				}
 				else if (begin>=0 && end<list.size()){
-					for(int i = begin;i<end;i++){
+					for(int i = begin;i<=end;i++){
 						System.out.println(list.get(i).toString());
 					}
+				}
+				else {
+					System.out.println("Il n'y a pas autant d'objet dans cette liste");
 				}
 			}
 			else {
@@ -142,6 +190,44 @@ public class TableHachage {
 		else {
 			System.out.println("La clé n'existe pas");
 			return 0;
+		}
+	}
+	
+	public void lPop(Object k){
+		if (h.containsKey(k)){
+			if (h.get(k) instanceof ArrayList){
+				ArrayList<Object> list = (ArrayList<Object>)h.get(k);
+				Object v = list.get(0);
+				list.remove(0);
+				h.remove(k);
+				h.put(k, list);
+				System.out.println("la valeur supprimé est :"+v.toString());
+			}
+			else {
+				System.out.println("Attention ce n'est pas une liste");
+			}
+		}
+		else {
+			System.out.println("La clé n'existe pas");
+		}
+	}
+	
+	public void rPop(Object k){
+		if (h.containsKey(k)){
+			if (h.get(k) instanceof ArrayList){
+				ArrayList<Object> list = (ArrayList<Object>)h.get(k);
+				Object v = list.get(list.size()-1);
+				list.remove(list.size()-1);
+				h.remove(k);
+				h.put(k, list);
+				System.out.println("la valeur supprimé est :"+v.toString());
+			}
+			else {
+				System.out.println("Attention ce n'est pas une liste");
+			}
+		}
+		else {
+			System.out.println("La clé n'existe pas");
 		}
 	}
 }
--- projetTAGL/src/test/java/GLM/AppTest.java
+++ projetTAGL/src/test/java/GLM/AppTest.java
@@ -1,5 +1,6 @@
 package GLM;
 
+import TAGLClient.TableHachage;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -34,5 +35,147 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+    
+    public void testValueOfString(){
+    	System.out.println("Value Of String :");
+    	TableHachage t = new TableHachage();
+    	String s = "1";
+    	String s1 = "-1";
+    	String s2 = "+1";
+    	String s3 = "15697";
+    	String s4 = "1.5465";
+    	String s5 = "test";
+    	String s6 = "125test";
+    	String s7 = "12.5test";
+    	assertEquals(0, t.valueOfString(s));
+    	assertEquals(0, t.valueOfString(s1));
+    	assertEquals(0, t.valueOfString(s2));
+    	assertEquals(0, t.valueOfString(s3));
+    	assertEquals(1, t.valueOfString(s4));
+    	assertEquals(-1, t.valueOfString(s5));
+    	assertEquals(-1, t.valueOfString(s6));
+    	assertEquals(-1, t.valueOfString(s7));
+    }
+    
+    public void testSetEtGet(){
+    	System.out.println("Set et Get :");
+    	TableHachage t = new TableHachage();
+    	t.set("test1", "test1");
+    	t.set("test2", "1");
+    	t.set("test3", "1.5618");
+    	t.set("test4", "15test15");
+    	t.set("test5", "15618");
+    	assertEquals("test1", t.get("test1"));
+    	assertEquals(1, t.get("test2"));
+    	assertEquals(1.5618,t.get("test3"));
+    	assertEquals("15test15", t.get("test4"));
+    	assertEquals(15618,t.get("test5"));
+    	assertEquals(0, t.get("test6"));
+    }
+    
+    public void testDel(){
+    	System.out.println("Del :");
+    	TableHachage t = new TableHachage();
+    	t.set("test1", "test1");
+    	t.set("test2", "1");
+    	t.set("test3", "1.5618");
+    	t.set("test4", "15test15");
+    	t.set("test5", "15618");
+    	t.del("test1");
+    	t.del("test2");
+    	t.del("test3");
+    	assertEquals(0, t.get("test1"));
+    	assertEquals(0, t.get("test2"));
+    	assertEquals(0,t.get("test3"));
+    	assertEquals("15test15", t.get("test4"));
+    	assertEquals(15618,t.get("test5"));
+    	assertEquals(0, t.get("test6"));
+    }
+    
+    public void testIncr(){
+    	System.out.println("Incr :");
+    	TableHachage t = new TableHachage();
+    	t.set("test1", "test1");
+    	t.set("test2", "1");
+    	t.set("test3", "1.5618");
+    	t.set("test4", "15test15");
+    	t.set("test5", "15618");
+    	t.incr("test1");
+    	t.incr("test2");
+    	t.incr("test3");
+    	t.incr("test4");
+    	t.incr("test5");
+    	assertEquals("test1", t.get("test1"));
+    	assertEquals(2, t.get("test2"));
+    	assertEquals(2.5618,t.get("test3"));
+    	assertEquals("15test15", t.get("test4"));
+    	assertEquals(15619,t.get("test5"));
+    	assertEquals(0, t.get("test6"));
+    }
+    
+    public void testRPush(){
+    	System.out.println("RPush :");
+    	TableHachage t = new TableHachage();
+    	t.rPush("test1", "test1");
+    	t.rPush("test1", "1");
+    	t.rPush("test1", "1.5618");
+    	assertEquals(3, t.lLen("test1"));
+    	t.lRange("test1", 0, -1);
+    }
+    
+    public void testLPush(){
+    	System.out.println("LPush :");
+    	TableHachage t = new TableHachage();
+    	t.lPush("test1", "test1");
+    	t.lPush("test1", "1");
+    	t.lPush("test1", "1.5618");
+    	assertEquals(3, t.lLen("test1"));
+    	t.lRange("test1", 0, -1);
+    }
+    
+    public void testRPop(){
+    	System.out.println("RPop :");
+    	TableHachage t = new TableHachage();
+    	t.rPush("test1", "test1");
+    	t.rPush("test1", "1");
+    	t.rPush("test1", "1.5618");
+    	t.rPop("test1");
+    	assertEquals(2, t.lLen("test1"));
+    	t.lRange("test1", 0, -1);
+    }
+    
+    public void testLPop(){
+    	System.out.println("LPop :");
+    	TableHachage t = new TableHachage();
+    	t.rPush("test1", "test1");
+    	t.rPush("test1", "1");
+    	t.rPush("test1", "1.5618");
+    	t.lPop("test1");
+    	assertEquals(2, t.lLen("test1"));
+    	t.lRange("test1", 0, -1);
+    }
+    
+    public void testLRangeEtLlen(){
+    	System.out.println("LRange et Llen :");
+    	TableHachage t = new TableHachage();
+    	t.rPush("test1", "test1");
+    	t.rPush("test1", "1");
+    	t.rPush("test1", "1.5618");
+    	t.lRange("test1", 0, -1);
+    	System.out.println();
+    	t.lRange("test1", 0, 1);
+    	System.out.println();
+    	t.lRange("test1", 0, 2);
+    	System.out.println();
+    	t.lRange("test1", 0, 3);
+    	System.out.println();
+    	assertEquals(3, t.lLen("test1"));
+    	t.rPop("test1");
+    	assertEquals(2, t.lLen("test1"));
+    	t.rPop("test1");
+    	assertEquals(1, t.lLen("test1"));
+    	t.rPop("test1");
+    	assertEquals(0, t.lLen("test1"));
     }
 }
